176ad0fc58ed50b26f40c57a1638d7d08403f102
sushantrajbhar <sushraj44@gmail.com>
2024-11-06 00:46:05 +0800

    completed model trainer pipeline

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -5,13 +5,28 @@ from sensor.logger import logging
 import sys , os 
 from sensor.components.data_ingestion import DataIngestion
 
+
 from sensor.components.data_validation import DataValidation
 
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig
+
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+
+
 from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+
 
 from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact,DataTransformationArtifact
 
 from sensor.components.data_transformation import DataTransformation
+
+
+
+
+
+from sensor.components.model_trainer import ModelTrainer
+from sensor.entity.artifact_entity import ModelTrainerArtifact
+from sensor.entity.config_entity import ModelTrainerConfig
 
 class TrainPipeline:
 
@@ -70,6 +85,15 @@ class TrainPipeline:
         except  Exception as e:
             raise  SensorException(e,sys)
         
+    def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact):
+        try:
+            model_trainer_config = ModelTrainerConfig(training_pipeline_config=self.training_pipeline_config)
+            model_trainer = ModelTrainer(model_trainer_config, data_transformation_artifact)
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+        except  Exception as e:
+            raise  SensorException(e,sys)
+
 
 
     def run_pipeline(self):
@@ -84,6 +108,9 @@ class TrainPipeline:
 
 
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
+
+
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact)  
 
             
         except Exception as e :    
