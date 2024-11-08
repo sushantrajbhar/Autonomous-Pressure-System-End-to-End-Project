a27ea3c5c04e73f41f8124e4f80f1358533f3ee3
sushantrajbhar <sushraj44@gmail.com>
2024-11-08 16:45:49 +0800

    completed fastapi application

--- main.py
+++ main.py
@@ -1,37 +1,132 @@
+from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
-import os
-import sys
+import os , sys
 from sensor.logger import logging
-from sensor.configuration.mongo_db_connection  import MongoDBClient
 
-from sensor.logger import logging
-from sensor.utils2 import dump_csv_file_to_mongodb_collection
+
 from sensor.pipeline.training_pipeline import TrainPipeline
+from sensor.utils.main_utils import load_object
+from sensor.ml.model.estimator import ModelResolver,TargetValueMapping
+from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import os,sys
+from sensor.logger import logging
+from sensor.pipeline import training_pipeline
+from sensor.pipeline.training_pipeline import TrainPipeline
+import os
+from sensor.utils.main_utils import read_yaml_file
+from sensor.constant.training_pipeline import SAVED_MODEL_DIR
+
+
+from  fastapi import FastAPI
+from sensor.constant.application import APP_HOST, APP_PORT
+from starlette.responses import RedirectResponse
+from uvicorn import run as app_run
+from fastapi.responses import Response
+from sensor.ml.model.estimator import ModelResolver,TargetValueMapping
+from sensor.utils.main_utils import load_object
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from fastapi import FastAPI, File, UploadFile, Response
+import pandas as pd
+
+TEST_CSV_PATH = "C:\Users\HP\Desktop\LML\LML\Projects\2. APS\APS_End_to_End\artifact\11_08_2024_14_20_18\data_transformation\transformed\train.npy
+app = FastAPI()
 
 
 
-# if __name__=="__main__":
-#     file_path=r"C:\Users\HP\Desktop\Learn Machine Learning\Learn Machine Learning\Projects for Resume\2. Autonomous Pressure System\Autonomous-pressure-system-End-to-End-project\Dataset\aps_failure_training_set1.csv"
-#     database_name="APS"
-#     collection_name="mongoclass"
-#     dump_csv_file_to_mongodb_collection(file_path, database_name, collection_name)
-
-# def test_exception():
-#     try:
-#         logging.info("Welcome to the project")
-#         a=1/0
-#     except Exception as e:
-#         raise SensorException(e,sys)
+origins = ["*"]
+#Cross-Origin Resource Sharing (CORS) 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-# if __name__=="__main__":
-#     try:
-#         test_exception()
-#     except Exception as e:
-#         print(e)
+@app.get("/",tags=["authentication"])
+async def  index():
+    return RedirectResponse(url="/docs")
+
+
+
+
+
+@app.get("/train")
+async def train():
+    try:
+
+        training_pipeline = TrainPipeline()
+
+        if training_pipeline.is_pipeline_running:
+            return Response("Training pipeline is already running.")
+        
+        training_pipeline.run_pipeline()
+        return Response("Training successfully completed!")
+    except Exception as e:
+        return Response(f"Error Occurred! {e}")
+        
+
+
+
+
+@app.get("/predict")
+async def predict():
+    try:      
+        # Load the test data from CSV file
+        df = pd.read_csv(TEST_CSV_PATH)
+
+        Model_resolver = ModelResolver(model_dir=SAVED_MODEL_DIR)
+        if not Model_resolver.is_model_exists():
+            return Response("Model is not available")
+        
+        best_model_path = Model_resolver.get_best_model_path()
+        model= load_object(file_path=best_model_path)
+        y_pred=model.predict(df)
+        df['predicted_column'] = y_pred
+        df['predicted_column'].replace(TargetValueMapping().reverse_mapping,inplace=True)
+
+
+
+    except  Exception as e:
+        raise  SensorException(e,sys)
+
+
+
+
+
+def main():
+    try:
+            
+        training_pipeline = TrainPipeline()
+        training_pipeline.run_pipeline()
+    except Exception as e:
+        print(e)
+        logging.exception(e)
+
+
 
 if __name__ == "__main__":
 
+    app_run(app ,host=APP_HOST,port=APP_PORT)
 
-    training_pipeline = TrainPipeline()
-    training_pipeline.run_pipeline()
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
